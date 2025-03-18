1c1d10289cc0e757cff5efaf88461d5ba425a3b8
MiFr <110558561+Mikester58@users.noreply.github.com>
2025-03-18 18:20:04 -0500

    Need to Update Code
    Doesnt currently reflect the csv sit

---
+++ PrepareData.py
@@ -0,0 +1,3 @@
+# The downloaded CSV (https://www.kaggle.com/datasets/jonathanpilafas/2024-march-madness-statistical-analysis)
+# was a little messy, It will need to be resorted by date so that the model doesnt accidentally train on the test data.
+
